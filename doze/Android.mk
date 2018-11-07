8974531bb56e0c367e70d7bb4db0b6a334b282ee
Manish4586 <Manish.n.manish45@gmail.com>
2018-11-07 20:16:31 +0530

    LOCAL_SDK_VERSION unspecified

--- doze/Android.mk
+++ doze/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
+#LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := XiaomiDoze
 LOCAL_CERTIFICATE := platform
@@ -19,8 +19,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-appcompat \
     android-support-v14-preference \
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+#LOCAL_STATIC_JAVA_LIBRARIES := \
+#    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
