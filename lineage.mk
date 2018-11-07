4a2bac5e215dfd351baacc89303c07f1312a9467
Manish4586 <Manish.n.manish45@gmail.com>
2018-11-07 18:28:12 +0530

    Fix out build error

--- lineage.mk
+++ lineage.mk
@@ -17,13 +17,13 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from jasmine device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product, device/xiaomi/jasmine/device_jasmine.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jasmine
