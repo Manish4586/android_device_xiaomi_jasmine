54bf51e152388bf5f2bf2fee73fd53448182a8d6
coderz <coderz35@gmail.com>
2018-11-08 10:05:54 +0200

    jasmine : again cleanup

--- lineage_jasmine.mk
+++ lineage_jasmine.mk
@@ -17,7 +17,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -25,11 +24,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from jasmine device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+PRODUCT_NAME := lineage_jasmine
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jasmine
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi A2
-PRODUCT_NAME := lineage_jasmine
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
