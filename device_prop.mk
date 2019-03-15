bcf087b5f9784e6853665c7896b7a7b6954489af
Manish4586 <manish.n.manish45@gmail.com>
2019-03-15 19:03:08 +0530

    update from V10.0.5.0.PDIMIXM

--- device_prop.mk
+++ device_prop.mk
@@ -63,7 +63,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vendor.camera.multicam.fpsmatch=TRUE \
 	persist.vendor.camera.multicam.framesync=1 \
 	persist.vendor.camera.enableAdvanceFeatures=0x347 \
-    persist.vendor.camera.expose.aux=1
+	persist.vendor.camera.expose.aux=1	
 	
 # Dual SIM
 PRODUCT_PROPERTY_OVERRIDES += \
