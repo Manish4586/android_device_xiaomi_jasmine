dfed53dce544a2078a9f1a3cf05284fb2b9f9d74
Manish4586 <manish.n.manish45@gmail.com>
2019-03-18 03:39:42 +0530

    fix safetynet leave arcore

--- aosp_jasmine_sprout.mk
+++ aosp_jasmine_sprout.mk
@@ -43,8 +43,6 @@ DEVICE_MAINTAINERS="Manish4586"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="jasmine_sprout" \
     PRODUCT_NAME="jasmine_sprout" \
-    PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.4.0.PDIMIXM release-keys"
+	PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.5.0.PDIMIXM release-keys"
 
-BUILD_FINGERPRINT := "xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.4.0.PDIMIXM:user/release-keys"
-
-#TARGET_VENDOR_PRODUCT_NAME := jasmine
+BUILD_FINGERPRINT := "xiaomi/jasmine/jasmine_sprout:9/PKQ1.180904.001/V10.0.5.0.PDIMIXM:user/release-keys"
