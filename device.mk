caba770c5de4a2bd45506d10f14f532449528c99
Manish4586 <manish.n.manish45@gmail.com>
2019-04-11 23:40:52 +0530

    jasmine: audio: fix stereo recording,dual mic
     Author: Manish4586 <manish.n.manish45@gmail.com>
     Date:      Thu Apr 11 23:40:52 2019 +0530
    
    Blame xiaomi for shipping devices with wrong audio configs.
    
    *remove useless fm strings
    *fix stereo recording
    *fix secondary mic
    
    	new file:   audio/audio/audio_policy_configuration.xml
    	modified:   audio/audio_platform_info.xml
    	modified:   audio/audio_policy_configuration.xml
    	modified:   audio/mixer_paths.xml
    	modified:   device.mk

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
 	$(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
 	$(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+	$(DEVICE_PATH)/audio/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
 	$(DEVICE_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
 	$(DEVICE_PATH)/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml
 
